391ad1b461d3191defdca8749baa0de956ec77a8
Raghu-Konda <raghukonda1906@gmail.com>
2020-11-01 18:04:34 +0530

    first commit

--- src/Gitpackage/Demo.java
+++ src/Gitpackage/Demo.java
@@ -2,7 +2,6 @@ package Gitpackage;
 
 public class Demo {
 
-	public static void main(String[] args) {
-		
-	}
 }
+
+
--- src/Gitpackage/Test.java
+++ src/Gitpackage/Test.java
@@ -1,8 +1,6 @@
 package Gitpackage;
 
 public class Test {
-	public static void main(String[] args) {
-		
-	}
+	
 
 }
